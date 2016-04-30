06af0848bf17eb74fd27ad23c280d4a3b7e7b41b
Louis S. Berman <louis@squideyes.com>
2016-04-30 15:40:25 -0400

    Add project files.

---
+++ RYAP/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace RYAP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contribute()
+        {
+            ViewBag.Message = "Your contribution page.";
+
+            return View();
+        }
+    }
+}
---
+++ RYAP/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("R-You A Pirate")]
+[assembly: AssemblyDescription("A compendium of pirate jokes")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RYAP.com")]
+[assembly: AssemblyCopyright("© 2006 to 2016 Marie Anne Chiment.  All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("860af63e-2b37-46bf-9de9-3e8550a66989")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
