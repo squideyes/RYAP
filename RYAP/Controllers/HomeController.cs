9ff8b15f1327cd7ee1bb322a9948cfce06929b9d
Louis S. Berman <louis@squideyes.com>
2016-04-30 22:48:27 -0400

    The home page needs some formatting but it's mostly done

--- RYAP/Controllers/HomeController.cs
+++ RYAP/Controllers/HomeController.cs
@@ -1,11 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using RYAP.Models;
+using System.Linq;
+using System.Web.Mvc;
+using PagedList;
 
 namespace RYAP.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private Entities db = new Entities();
+
+        public object Index(int? page)
         {
+            var pageNumber = page ?? 1;
+
+            var jokes = db.Jokes.OrderBy(j => j.AddedOn);
+
+            var onePageOfJokes = jokes.ToPagedList(pageNumber, 10);
+
+            ViewBag.OnePageOfJokes = onePageOfJokes;
+
             return View();
         }
 
